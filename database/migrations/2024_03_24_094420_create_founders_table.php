494e41f00a491f72842f5fd6f96a5a42f3d8cd31
Hussain Aminu <hussain_aminu@yahoo.com>
2024-03-24 11:25:38 +0300

    WIP

---
+++ database/migrations/2024_03_24_094420_create_founders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('founders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('company_name')->nullable();
+            $table->string('company_website')->nullable();
+            $table->string('company_phone')->nullable();
+            $table->string('company_address')->nullable();
+            $table->string('company_city')->nullable();
+            $table->string('company_state')->nullable();
+            $table->string('company_zip')->nullable();
+            $table->string('company_country')->nullable();
+            $table->string('status')->default(\App\Status::PENDING->value)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('founders');
+    }
+};
