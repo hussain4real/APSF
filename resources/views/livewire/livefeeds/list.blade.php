faad596f16c0f10c13959228a6c765bb63743101
hussain4real <aminuhussain22@gmail.com>
2024-04-22 15:18:37 +0300

    WIP

--- resources/views/livewire/livefeeds/list.blade.php
+++ resources/views/livewire/livefeeds/list.blade.php
@@ -50,7 +50,7 @@ new
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
     @foreach ($livefeeds as $livefeed)
-        <div class="p-6 flex space-x-2 bg-blue-300" wire:key="{{ $livefeed->id }}">
+        <div class="p-6 flex space-x-2 " wire:key="{{ $livefeed->id }}">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none"
                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
