2c65ae79145d41c590ab00b8c7fb5a9b166685ef
Hussain Aminu <hussain_aminu@yahoo.com>
2024-04-26 22:59:54 +0300

    WIP

--- app/Http/Middleware/RedirectIfSubscribed.php
+++ app/Http/Middleware/RedirectIfSubscribed.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-class RedirectIfSubscribed
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        if($request->user() && $request->user()->subscribed()){
-            // This user is a paying customer...
-            return redirect(route('filament.admin.pages.my-profile'));
-        }
-        return $next($request);
-    }
-}
+//
+//namespace App\Http\Middleware;
+//
+//use Closure;
+//use Illuminate\Http\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//class RedirectIfSubscribed
+//{
+//    /**
+//     * Handle an incoming request.
+//     *
+//     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+//     */
+//    public function handle(Request $request, Closure $next): Response
+//    {
+//        if($request->user() && $request->user()->subscribed()){
+//            // This user is a paying customer...
+//            return redirect(route('filament.admin.pages.my-profile'));
+//        }
+//        return $next($request);
+//    }
+//}
