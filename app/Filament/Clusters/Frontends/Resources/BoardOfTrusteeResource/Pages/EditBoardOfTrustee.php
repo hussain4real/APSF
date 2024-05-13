b7fdfb71b2f6285e16d3eb2acecdcf7fe7f7f7e0
Hussain Aminu <hussain_aminu@yahoo.com>
2024-05-13 10:59:18 +0300

    WIP

--- app/Filament/Clusters/Frontends/Resources/BoardOfTrusteeResource/Pages/EditBoardOfTrustee.php
+++ app/Filament/Clusters/Frontends/Resources/BoardOfTrusteeResource/Pages/EditBoardOfTrustee.php
@@ -4,8 +4,6 @@ namespace App\Filament\Clusters\Frontends\Resources\BoardOfTrusteeResource\Pages
 
 use App\Filament\Clusters\Frontends\Resources\BoardOfTrusteeResource;
 use Filament\Actions;
-use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
@@ -26,26 +24,8 @@ class EditBoardOfTrustee extends EditRecord
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                TextInput::make('name')
-                    ->label(__('Name'))
-                    ->required(),
-                SpatieMediaLibraryFileUpload::make('attachment')
-                    ->collection('board_of_trustees_images')
-                    ->hiddenLabel()
-                    ->responsiveImages()
-                    ->maxSize(1024 * 3)
-                    ->hint(__('Maximum size: 3MB.'))
-                    ->hintIcon('heroicon-o-information-circle')
-                    ->hintColor('warning')
-                    ->imagePreviewHeight('150')
-                    ->openable()
-                    ->preserveFilenames()
-                    ->downloadable()
-                    ->imageEditor(2)
-                    ->imageEditorEmptyFillColor('#dda581')
-                    ->uploadingMessage(__('uploading, please wait...')),
-            ]);
+        $createForm = new CreateBoardOfTrustee();
+
+        return $createForm->form($form);
     }
 }
