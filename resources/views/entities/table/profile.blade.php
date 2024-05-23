e967b9e5a6979b9002cf988e7bce72e269f9b19b
Hussain Aminu <hussain_aminu@yahoo.com>
2024-05-23 08:27:04 +0300

    WIP

--- resources/views/entities/table/profile.blade.php
+++ resources/views/entities/table/profile.blade.php
@@ -1,6 +1,6 @@
 <!-- https://gist.github.com/goodreds/5b8a4a2bf11ff67557d38c5e727ea86c -->
 
-<div class="max-w-2xl sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm mt-4 bg-white shadow-md rounded-lg text-gray-900">
+<div class="max-w-2xl sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-xl mt-4 bg-white shadow-md rounded-lg text-gray-900">
     <div class="rounded-t-lg h-32 overflow-hidden">
         <img class="object-cover object-top w-full" src='https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Mountain'>
     </div>
@@ -66,3 +66,29 @@
         </span>
     </div>
 </div>
+
+
+{{--sample cards for courses--}}
+{{--<div class="max-w-sm w-full lg:max-w-full lg:flex">--}}
+{{--    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">--}}
+{{--    </div>--}}
+{{--    <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">--}}
+{{--        <div class="mb-8">--}}
+{{--            <p class="text-sm text-gray-600 flex items-center">--}}
+{{--                <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">--}}
+{{--                    <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />--}}
+{{--                </svg>--}}
+{{--                Members only--}}
+{{--            </p>--}}
+{{--            <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>--}}
+{{--            <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>--}}
+{{--        </div>--}}
+{{--        <div class="flex items-center">--}}
+{{--            <img class="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink">--}}
+{{--            <div class="text-sm">--}}
+{{--                <p class="text-gray-900 leading-none">Jonathan Reinink</p>--}}
+{{--                <p class="text-gray-600">Aug 18</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
