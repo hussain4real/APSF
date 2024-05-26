73872a043a4799a4e1962f2847d7c49fe9af2bb7
Hussain Aminu <hussain_aminu@yahoo.com>
2024-05-26 13:55:49 +0300

    WIP

--- app/Filament/Clusters/Resources/Resources/TrainingProgramResource.php
+++ app/Filament/Clusters/Resources/Resources/TrainingProgramResource.php
@@ -14,7 +14,7 @@ class TrainingProgramResource extends Resource
 {
     protected static ?string $model = TrainingProgram::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
 
     protected static ?string $cluster = Resources::class;
 
