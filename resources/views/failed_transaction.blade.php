b10d77c695a51e4b0d8b9977b584b9215f8fb54a
Hussain Aminu <hussain_aminu@yahoo.com>
2024-06-11 08:34:55 +0300

    Update transaction amount default value to 1 in Pay2MController

--- resources/views/failed_transaction.blade.php
+++ resources/views/failed_transaction.blade.php
@@ -4,8 +4,29 @@
         <h5 class="card-header bg-danger">Notice</h5>
         <div class="card-body">
             <h5 class="card-title alert alert-danger">{{session('error')}}</h5>
-            <p class="card-text">{{ __('We could not process your transaction') }}</p>
+
             <a href="{{route('subscribe')}}" class="btn btn-primary">Try again</a>
+        </div>
+    </div>
+    @endif
+    @if (session()->has('info'))
+    <div id="card" class="card text-center border-info mx-auto" style="width: 30rem;">
+        <h5 class="card-header bg-info">Notice</h5>
+        <div class="card-body">
+            <h5 class="card-title alert alert-info">{{session('info')}}</h5>
+
+            <address>
+                <strong>Contact Information:</strong><br>
+                <a href="mailto:info@arab-psf.com">
+                    <i class="fas fa-envelope"></i>
+                    <span> info@arab-psf.com </span>
+                </a>
+                <!--phone number -->
+                <a href="tel:+968 99871199">
+                    <i class="fas fa-phone"></i>
+                    <span> +968 99871199 </span>
+                </a>
+            </address>
         </div>
     </div>
     @endif
