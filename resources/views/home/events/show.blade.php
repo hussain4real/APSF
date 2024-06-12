da6cddcc639dae2a723a86f6fb7b3c42de5a42ba
Hussain Aminu <hussain_aminu@yahoo.com>
2024-06-12 13:38:34 +0300

    WIP

--- resources/views/home/events/show.blade.php
+++ resources/views/home/events/show.blade.php
@@ -33,8 +33,8 @@
                     </div>
                 @empty
                     <div class="empty-image-container">
-                        @if($firstImage)
-                            <img src="{{$firstImage->getUrl()}}" alt="">
+                        @if($randomImage)
+                            <img src="{{$randomImage->getUrl()}}" alt="">
                         @else
                             <img src="{{asset('assets/imgs/apsf/news-updates/news-1.webp')}}" alt="">
                         @endif
@@ -176,6 +176,7 @@
         width: 100%;
         height: 35rem;
         object-fit: cover;
+        object-position: center center;
         border-radius: 1rem;
 
     }
