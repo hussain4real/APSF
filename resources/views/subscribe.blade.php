51196562f73326551af548e7426086062c1801db
Hussain Aminu <hussain_aminu@yahoo.com>
2024-06-19 15:00:19 +0300

    Update payment response route to use GET instead of POST

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -62,6 +62,7 @@
     {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">--}}
     <div class="container mx-auto">
 
+        https://arab-psf.com/payment-response
         <form id='pay2m_payment_form' name='pay2m-payment-form' method='post' action="https://pay.pay2m.com/Ecommerce/api/Transaction/PostTransaction" hidden="true">
             {{-- @csrf--}}
             <!-- ... -->
